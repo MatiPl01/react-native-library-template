aa5284561d9e20b90083251d545b5f9a2cf80559
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-01-26 13:11:58 +0100

    fix: Add missing package name change functionality (#32)
    * Add missing package name change functionality
    
    * Add changes in the initialization script
    
    * Change function name

--- bin/init.js
+++ bin/init.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import degit from 'degit';
 import logger from './logger.js';
+import { readJSON, writeJSON } from './utils.js';
 
 const AUTHOR = 'MatiPl01';
 const REPOSITORY = 'react-native-library-template';
@@ -24,8 +25,21 @@ const init = (projectName, verbose = false, directory = '.') => {
 
   const projectPath = path.resolve(directory, projectName);
 
-  emitter.clone(projectPath).then(() => {
-    logger.success('Template initialized successfully!');
+  emitter.clone(projectPath).then(async () => {
+    if (verbose) {
+      logger.info(
+        'Template files were copied. Changing name in package.json...'
+      );
+    }
+    const packageJsonPath = path.resolve(projectPath, 'package.json');
+    const packageJson = readJSON(packageJsonPath);
+    packageJson.name = projectName;
+    writeJSON(packageJsonPath, packageJson);
+    if (verbose) {
+      logger.info(`Library name was set to '${projectName}' in package.json`);
+    }
+
+    logger.success('Temaplate initialization completed successfully!');
   });
 };
 
