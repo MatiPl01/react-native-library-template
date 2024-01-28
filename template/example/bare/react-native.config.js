51397196a3410b5a1dbcf01657bec8e5882a50de
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-01-28 17:06:22 +0100

    Fix resolution of dependencies added in the library dependencies list

--- template/example/bare/react-native.config.js
+++ template/example/bare/react-native.config.js
@@ -1,13 +1,20 @@
 /**
  * This file is required to properly resolve native dependencies
- * listed in the example/app package.json on Android.
  */
 const path = require('path');
-const appPackageJSON = require('../app/package.json');
+
+const rootDir = path.resolve(__dirname, '../..');
+const rootPkg = require(path.join(rootDir, 'package.json'));
+
+const appDir = path.resolve(__dirname, '../app');
+const appPkg = require(path.resolve(appDir, 'package.json'));
 
 let dependencies = {};
 
-Object.keys(appPackageJSON.dependencies).forEach(dep => {
+[
+  ...Object.keys(appPkg.dependencies || {}),
+  ...Object.keys(rootPkg.dependencies || {})
+].forEach(dep => {
   dependencies[dep] = {
     root: path.resolve(__dirname, `../../node_modules/${dep}`)
   };
