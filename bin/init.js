b812a692ec5f94249fa0ce8de8b06949d79ce748
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-01-28 01:06:14 +0100

    feat: Add automatic dependencies installation after setup (#34)
    Add automatic dependencies installation after setup

--- bin/init.js
+++ bin/init.js
@@ -2,10 +2,33 @@ import path from 'path';
 import degit from 'degit';
 import logger from './logger.js';
 import { readJSON, writeJSON } from './utils.js';
+import { execSync } from 'child_process';
 
 const AUTHOR = 'MatiPl01';
 const REPOSITORY = 'react-native-library-template';
 const TEMPLATE_DIR = 'template';
+
+const renamePackage = (projectPath, projectName, verbose) => {
+  if (verbose) {
+    logger.info(`Setting library name to '${projectName}'...`);
+  }
+  const packageJsonPath = path.resolve(projectPath, 'package.json');
+  const packageJson = readJSON(packageJsonPath);
+  packageJson.name = projectName;
+  writeJSON(packageJsonPath, packageJson);
+  if (verbose) {
+    logger.info(`Library name was set to '${projectName}' in package.json`);
+  }
+};
+
+const installPackages = (projectPath, verbose) => {
+  if (verbose) {
+    logger.info('Installing packages...');
+  }
+  process.chdir(projectPath);
+  execSync('yarn install');
+  logger.info('Packages were installed');
+};
 
 const init = (projectName, verbose = false, directory = '.') => {
   logger.info(`Initializing a new project: ${projectName}`);
@@ -27,19 +50,12 @@ const init = (projectName, verbose = false, directory = '.') => {
 
   emitter.clone(projectPath).then(async () => {
     if (verbose) {
-      logger.info(
-        'Template files were copied. Changing name in package.json...'
-      );
+      logger.info('Template files were copied.');
     }
-    const packageJsonPath = path.resolve(projectPath, 'package.json');
-    const packageJson = readJSON(packageJsonPath);
-    packageJson.name = projectName;
-    writeJSON(packageJsonPath, packageJson);
-    if (verbose) {
-      logger.info(`Library name was set to '${projectName}' in package.json`);
-    }
-
-    logger.success('Temaplate initialization completed successfully!');
+    renamePackage(projectPath, projectName, verbose);
+    logger.success('Temaplate was successfully cloned!');
+    installPackages(projectPath, verbose);
+    logger.success('🎉 Project was successfully initialized!');
   });
 };
 
