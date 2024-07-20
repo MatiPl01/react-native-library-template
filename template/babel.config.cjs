c963e6e39bceec0d2c1c526d7a764550b24cf312
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-07-20 11:50:15 +0200

    Remove reanimated babel plugin

--- template/babel.config.cjs
+++ template/babel.config.cjs
@@ -2,5 +2,5 @@
  * Modify this babel config file when you need to add a new plugin.
  */
 module.exports = {
-  plugins: ['react-native-reanimated/plugin']
+  /* Add your babel plugins/presets here */
 };
